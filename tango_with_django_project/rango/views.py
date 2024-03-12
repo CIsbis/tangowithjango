8c3c10f285e67af9748b5d7b3ba2355332375f8a
Cameron Isbister <2955880I@student.gla.ac.uk>
2024-03-12 13:32:18 +0000

    push1

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says FRONT ENDS FOR LOSERS")
+    return HttpResponse(
+        "<h1>Rango says FRONT ENDS FOR LOSERS</h1>"
+        "<a href='/rango/about/'>About</a>"
+    )
+
+
+def about(request):
+    return HttpResponse(
+        "<h1>Rango says it all about us</h1>"
+        "<a href='/rango/'>Index</a>"
+    )
